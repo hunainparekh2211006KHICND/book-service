fc57d129ab028d69b7f234695cb76ef333dd0ba2
hunainparekh2211006KHICND <hunain.parekh@xloopdigital.com>
2022-12-22 11:34:44 +0500

    change the port

--- src/main/java/com/glc/bookservice/BookServiceApplication.java
+++ src/main/java/com/glc/bookservice/BookServiceApplication.java
@@ -1,5 +1,7 @@
 package com.glc.bookservice;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookServiceApplication.class, args);
+		SpringApplication app =new SpringApplication(BookServiceApplication.class);
+		app.setDefaultProperties(Collections
+		.singletonMap("server.port", "5000"));
+	  	app.run(args);
 	}
 
 }
